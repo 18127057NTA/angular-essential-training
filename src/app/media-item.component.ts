40179123ec4c208d24c98646e4dab1768e7ab073
myus3rnam3159 <18127057@student.hcmus.edu.vn>
2022-05-27 13:30:14 +0700

    update app.component + media-item

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
@@ -7,8 +7,11 @@ import { Component, Input } from '@angular/core';
 })
 export class MediaItemComponent {
   @Input() mediaItem;
+  @Output() delete = new EventEmitter();
 
   onDelete() {
     console.log('deleted');
+    //Xóa một mediaItem
+    this.delete.emit(this.mediaItem);
   }
 }
