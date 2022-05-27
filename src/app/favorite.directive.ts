ec510562212dc8a53acbc67aa6f633ad9555209c
myus3rnam3159 <18127057@student.hcmus.edu.vn>
2022-05-27 21:15:19 +0700

    update favorite.directive.ts

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,10 +1,18 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
+  //Những chuỗi trong ngoặc đơn của decorator là built-in event/css selector của angular
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+  @HostListener('mouseenter') onMouseEnter() {
+    this.hovering = true;
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    this.hovering = false;
+  }
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
