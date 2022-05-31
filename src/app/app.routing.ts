0895c405e0c34ab11919cd763cbdc038e759a8d1
myus3rnam3159 <18127057@student.hcmus.edu.vn>
2022-05-31 21:26:34 +0700

    update app module, app routing, new-item routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { MediaItemListComponent } from './media-item-list.component';
 
+//Hàm import trả về một promise -> gọi then method để take action after promise đó resolved
+// Trong then, vì ta muốn trả về loaded module -> sử dụng arrow function, đặt tên parameter là m: module
+
 const appRoutes: Routes = [
+  {path: 'add', loadChildren: () => import('./new-item/new-item.module').then(m => m.NewItemModule)},
   { path: ':medium', component: MediaItemListComponent },
   { path: '', pathMatch: 'full', redirectTo: 'all' }
 ];
